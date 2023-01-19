e227f1d373d04c408a84d76076c5bc00bc0dae9e
primado <primadoprincepa@gmail.com>
2023-01-19 16:36:21 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+import Hero from './Hero'
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <Hero />
+      
+    </div>
+  )
+}
+
+export default App
