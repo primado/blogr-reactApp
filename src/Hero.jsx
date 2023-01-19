66899d6629d98db5980ee18273084c1b7b9a88b2
primado <primadoprincepa@gmail.com>
2023-01-19 18:27:21 +0000

    after initial commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,3 +1,28 @@
 import React from "react";
 
+// Images
+import logo from "../images/logo.svg";
 
+export default function Hero() { 
+
+    return (
+        <div className="hero">
+            
+            <header className="bg-light-red">
+                <nav>
+                    <div className="nav-right">
+                        <div className="nav-brand">
+                            <img src={logo} alt="brand logo" className="" />
+                        </div>
+
+                        
+                        <div className="nav-items">
+                            
+                        </div>
+                    </div>
+                </nav>
+            </header>
+
+        </div>
+    )
+}
